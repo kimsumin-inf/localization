1327de6b82208778eb643eb7ae294d044e116b02
sumin <ksm4465ksm@chungbuk.ac.kr>
2022-07-18 22:50:07 +0900

    Changes

--- include/localization/localization.h
+++ include/localization/localization.h
@@ -16,7 +16,7 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/Float64.h>
 
-#include <novatel_gps_msgs/NovatelVelocities.h>
+#include <novatel_gps_msgs/NovatelVelocity.h>
 #include <novatel_gps_msgs/NovatelPosition.h>
 
 #include <tf/transform_broadcaster.h>
@@ -31,11 +31,19 @@
 
 class Localization{
 private:
+
     void gps_CB(const sensor_msgs::NavSatFix::ConstPtr& msg);
     void bestvel_CB(const novatel_gps_msgs::NovatelVelocity::ConstPtr& msg);
     void bestpos_CB(const novatel_gps_msgs::NovatelPosition::ConstPtr& msg);
 
     void imu_CB(const sensor_msgs::Imu::ConstPtr& msg);
+
+    int encvel_Scale = 10;
+    int encsteer_Scale = 71;
+    void encvel_CB(const std_msgs::Int16::ConstPtr&msg);
+    void encSteer_CB(const std_msgs::Int16::ConstPtr&msg);
+
+
 
     ros::Time time;
     ros::NodeHandle nh;
@@ -47,13 +55,34 @@ private:
 
     ros::Subscriber subIMU;
 
-    sensor_msgs::NavSatFix GPS;
-    sensor_msgs::Imu IMU;
+    ros::Subscriber subEncVel;
+    ros::Subscriber subEncSteer;
+
+    sensor_msgs::NavSatFix gps;
+    sensor_msgs::Imu imu;
     novatel_gps_msgs::NovatelVelocity bestVel;
     novatel_gps_msgs::NovatelPosition bestPos;
+    std_msgs::Int16 ENC_VEL;
+    std_msgs::Int16 ENC_Steer;
+
+    struct GPS{
+        enum IDX{
+            X=0,
+            Y=1,
+            YAW=2,
+            V=3
+        };
+    };
+    typedef GPS::IDX GPSIDX;
+    Eigen::MatrixXd gpsData;
+
+    double imu_prev;
+    double imu_now;
+    Eigen::MatrixXd NED = Eigen::MatrixXd(3,1);
 
     double utm_x;
     double utm_y;
+
 public:
     Localization();
 };
--- src/localization.cpp
+++ src/localization.cpp
@@ -9,13 +9,17 @@ Localization::Localization()
     std::string gps_bestvel_subscribe_topic_name;
     std::string gps_bestpos_subscribe_topic_name;
     std::string imu_subscribe_topic_name;
-
+    std::string Enc_Vel_subscribe_topic_name;
+    std::string Enc_Steer_subscribe_topic_name;
     //launch file parameters
     pnh.param<std::string>("gps_subscribe_topic_name",gps_subscribe_topic_name,"/fix");
     pnh.param<std::string>("gps_bestvel_subscribe_topic_name",gps_bestvel_subscribe_topic_name,"/bestvel");
     pnh.param<std::string>("gps_bestpos_subscribe_topic_name",gps_bestpos_subscribe_topic_name,"/bestpos");
 
     pnh.param<std::string>("imu_subscribe_topic_name", imu_subscribe_topic_name, "/vectornav/IMU");
+
+    pnh.param<std::string>("Enc_Vel_subscribe_topic_name", Enc_Vel_subscribe_topic_name, "/MSG_CON/Rx_Vel");
+    pnh.param<std::string>("Enc_Steer_subscribe_topic_name", Enc_Steer_subscribe_topic_name, "/MSG_CON/Rx_Steer");
 
     pnh.param<double>("utm_x",utm_x,302533.174487);
     pnh.param<double>("utm_y",utm_y,4124215.346310);
@@ -25,18 +29,50 @@ Localization::Localization()
     subBestVel = nh.subscribe(gps_bestvel_subscribe_topic_name,1,&Localization::bestvel_CB,this);
 
     subIMU = nh.subscribe(imu_subscribe_topic_name,1,&Localization::imu_CB,this);
+
+    subEncVel = nh.subscribe(Enc_Vel_subscribe_topic_name,1,&Localization::encvel_CB,this);
+    subEncSteer = nh.subscribe(Enc_Steer_subscribe_topic_name,1, &Localization::encSteer_CB,this);
+
 }
 
 void Localization::gps_CB(const sensor_msgs::NavSatFix::ConstPtr &msg) {
+    gps = *msg;
 
 }
 
 void Localization::bestvel_CB(const novatel_gps_msgs::NovatelVelocity::ConstPtr& msg){
-
+    bestVel = *msg;
 }
 void Localization::bestpos_CB(const novatel_gps_msgs::NovatelPosition::ConstPtr &msg) {
-
+    bestPos = *msg;
 }
 void Localization::imu_CB(const sensor_msgs::Imu::ConstPtr &msg) {
+    imu = *msg;
+    imu_now = ros::Time::now().toSec();
+    double r,p,y;
+    tf::Quaternion rpy;
+    tf::quaternionMsgToTF(imu.orientation, rpy);
+    tf::Matrix3x3(rpy).getRPY(r,p,y);
+    //y-=1.58;
+
+    NED(0,0) = (ENC_VEL.data/encvel_Scale) * cos(r);
+    NED(1,0) = (ENC_VEL.data/encvel_Scale) * sin(r);
+    NED(2,0) = 0;
+    double dt = double(imu_now-imu_prev);
+
+    cout <<NED<<endl;
+    cout << r<< " " << p<<" " << y<<endl;
+    cout <<"************************************************"<<endl;
+
+    imu_prev = imu_now;
+
+}
+void Localization::encvel_CB(const std_msgs::Int16::ConstPtr &msg) {
+    ENC_VEL = *msg;
+    //cout << ENC_VEL.data/encvel_Scale <<endl;
+}
+void Localization::encSteer_CB(const std_msgs::Int16::ConstPtr &msg) {
+    ENC_Steer = *msg;
+    //cout << ENC_Steer.data/encsteer_Scale<<endl;
 
 }
